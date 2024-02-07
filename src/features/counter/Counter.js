2b2c66b98d2e224159244c01506fe46f52d300bb
vidya-piske <vidyapiske123@gmail.com>
2024-02-07 20:38:07 +0530

    redux-commit

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, decrement } from './counterSlice';
+
+const Counter = () => {
+  const count = useSelector((state)=>state.counter.count);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+     <p>{count}</p>
+     <button onClick={()=>dispatch(increment())}>+</button>
+     <button onClick={()=>dispatch(decrement())}>-</button>
+    </div>
+  )
+}
+
+export default Counter
---
+++ src/features/counter/counterSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    count: 0
+}
+
+export const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment: (state) => {
+
+            state.count += 1;
+        },
+        decrement: (state) => {
+            if(state.count > 0){
+                state.count -= 1;
+            }
+           return state;
+        }
+    }
+});
+
+export const {increment, decrement} = counterSlice.actions;
+
+export default counterSlice.reducer;
